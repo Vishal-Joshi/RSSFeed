f83eb7f9895664ba0b8659b4948a8b65007c6b2a
vishal <joshivishal2010@gmail.com>
2017-03-20 00:50:00 +0900

    Removed bean configuration separately for Feed converter

--- src/main/java/com/uzabase/assignment/RSSFeedConfiguration.java
+++ src/main/java/com/uzabase/assignment/RSSFeedConfiguration.java
@@ -1,7 +1,5 @@
 package com.uzabase.assignment;
 
-import com.uzabase.assignment.converter.ExcludeWordConverter;
-import com.uzabase.assignment.converter.FeedConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,11 +10,6 @@ public class RSSFeedConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean("excludefeedconverter")
-    public FeedConverter feedConverter() {
-        return new ExcludeWordConverter();
     }
 
 }
--- src/main/java/com/uzabase/assignment/converter/ExcludeWordConverter.java
+++ src/main/java/com/uzabase/assignment/converter/ExcludeWordConverter.java
@@ -1,5 +1,10 @@
 package com.uzabase.assignment.converter;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("excludefeedconverter")
 public class ExcludeWordConverter implements FeedConverter {
     @Override
     public String convertFeed(String feed, String wordToExclude) {
